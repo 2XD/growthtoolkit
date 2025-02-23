8c96e65b7ba54f8c652d5b5d7a046d07f85891a1
vm <81333771+2XD@users.noreply.github.com>
2025-02-22 19:51:17 -0600

    Update financialprojectioncalculator.py
    added month to month calculations for growth rate and then average

--- financialprojectioncalculator.py
+++ financialprojectioncalculator.py
@@ -1,10 +1,15 @@
+
+#This calculator uses expenses and revenues to determine the growth rate and give a financial projection
+#It calculates the growth rate seperately from month to month and adds them all up
+#Then the calculator divides by the number of months to get the most accurate result possible
+
 import csv
 
-#make sure that any input the user makes won't break the program.
+# Make sure that any input the user makes won't break the program.
 def get_valid_float(prompt):
     while True:
         value = input(prompt).strip()
-        
+
         if not value:
             print("Please enter a number.")
             continue
@@ -21,43 +26,48 @@ def get_valid_float(prompt):
 print("Welcome! Let's calculate your Financial Projections ")
 
 rev = get_valid_float("Enter your current monthly revenue: ")
-rev = float(rev)
 expenses = get_valid_float("Enter your current monthly expenses: ")
-expenses = float(expenses)
 
 profit = rev - expenses
 print("\nNow we need some past revenue data in order to calculate your growth rate.")
 
 months_ago = 0
 while months_ago <= 0:
-    months_ago = int(get_valid_float("How many months ago of revenue data do you want to use? "))
+    months_ago = get_valid_float("How many months ago of revenue data do you want to use? ")
+    months_ago = int(months_ago)  # Convert to integer after validation
     if months_ago <= 0:
         print("Enter a valid number (must be greater than 0).")
 
 rev_data = []
+exp_data = []  # Added for expenses
+
 while len(rev_data) < months_ago:
     rev_input = get_valid_float("Enter revenue for month " + str(len(rev_data) + 1) + ": ")
     rev_data.append(rev_input)
+    exp_input = get_valid_float("Enter expenses for month " + str(len(exp_data) + 1) + ": ")  # Added prompt for expenses
+    exp_data.append(exp_input)  # Added expense data storage
 
-if len(rev_data) == 0:
-    print("Assuming revenue for this period was 0 because nothing was entered.")
-    rev_data.append(0)
+# Calculate growth rate for each month and average them
+growth_rates = []
+for i in range(1, len(rev_data)):
+    growth_rate = ((rev_data[i] - rev_data[i-1]) / rev_data[i-1]) * 100  # Month-to-month growth rate
+    growth_rates.append(growth_rate)
+
+# Average the growth rates
+if growth_rates:
+    avg_growth_rate = sum(growth_rates) / len(growth_rates)
 else:
-    prev = rev_data[-1]
+    avg_growth_rate = 0  # If there's no growth data, set to 0%
 
-    if prev == 0:
-        gr = 0
-    else:
-        gr = ((rev - prev) / prev) * 100
+print("\nAverage growth rate calculated: " + str(round(avg_growth_rate, 2)) + "%" + " based on the last " + str(months_ago) + " months of data.")
 
-print("\nGrowth rate calculated: " + str(round(gr, 2)) + "%" + " based on the last " + str(months_ago) + " months of data.")
-
+# Allow user to override growth rate if desired
 newgr = input("\nWant to override this? Enter a new growth rate or just press Enter to continue with the calculated amount: ").strip()
 if newgr != "":
     try:
-        gr = float(newgr)
-    except:
-        print("The input wasn't a number, so we're sticking with " + str(round(gr, 2)) + "%.")
+        avg_growth_rate = float(newgr)
+    except ValueError:  # More specific exception
+        print("The input wasn't a number, so we're sticking with " + str(round(avg_growth_rate, 2)) + "%.")
 
 #do the calculations that will be saved to the csv later
 check = 0
@@ -70,11 +80,11 @@ while check == 0:
     else:
         print("Enter a non-zero number value.")
 
-projrev = rev * (1 + gr / 100) ** futuremonths
-projexp = expenses * (1 + gr / 100) ** futuremonths
+projrev = rev * (1 + avg_growth_rate / 100) ** futuremonths
+projexp = expenses * (1 + avg_growth_rate / 100) ** futuremonths
 projprofit = projrev - projexp
 
-#save as a csv
+# Save as a CSV
 file_name = input("What do you want to save the file as?: ")
 if file_name == "":
     print("No input detected. Saving as 'financialprojectioncalculation.csv'.")
@@ -84,9 +94,9 @@ csv_filename = file_name + ".csv"
 
 with open(csv_filename, "w", newline="") as file:
     writer = csv.writer(file)
-    headers = ["Revenue", "Expenses", "Profit", "Months Ago", "Previous Revenue", "Growth Rate", "Projected Revenue", "Projected Expenses", "Projected Profit"]
+    headers = ["Revenue", "Expenses", "Profit", "Months Ago", "Growth Rate", "Projected Revenue", "Projected Expenses", "Projected Profit"]
     writer.writerow(headers)
-    writer.writerow([rev, expenses, profit, months_ago, prev, gr, projrev, projexp, projprofit])
+    writer.writerow([rev, expenses, profit, months_ago, avg_growth_rate, projrev, projexp, projprofit])
 
-print("\n Data saved successfully to " + csv_filename + "!")
-print("The csv file will contain the revenue, expenses, profit, months ago, previous revenue that you entered,\n The growth rate, projected revenue, projected expenses, and projected profit were all calculated by the Financial Calculator.")
+print("\nData saved successfully to " + csv_filename + "!")
+print("The csv file will contain the revenue, expenses, profit, and months ago that you entered,\nThe growth rate and the projected revenue, expenses, and profit were calculated by the Financial Calculator.")
